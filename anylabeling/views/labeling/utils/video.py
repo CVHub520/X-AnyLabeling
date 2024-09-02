70345c67574c5af46e63c9cbc63db847221183cc
cvhub <cv_hub@163.com>
2024-09-02 19:02:05 +0800

    [Optimize] Modularize code, improve error handling, and streamline user interactions

--- anylabeling/views/labeling/utils/video.py
+++ anylabeling/views/labeling/utils/video.py
@@ -11,35 +11,27 @@ from PyQt5.QtWidgets import (
 )
 
 
-def extract_frames_from_video(parent, source_video_path):
-    # Get the directory of the source video file
-    video_dir = osp.dirname(source_video_path)
+def get_output_directory(source_video_path):
+    video_dir = os.path.dirname(source_video_path)
+    folder_name = os.path.splitext(os.path.basename(source_video_path))[0]
+    output_dir = os.path.join(video_dir, folder_name)
+    return output_dir
 
-    # Create a folder in the current directory with the current video file_name
-    folder_name = osp.splitext(osp.basename(source_video_path))[0]
-    output_dir = osp.join(video_dir, folder_name)
-
-    if osp.exists(output_dir):
-        # Ask the user if they want to overwrite the existing directory
+def ask_overwrite_directory(parent, output_dir):
+    if os.path.exists(output_dir):
         reply = QMessageBox.question(
             parent,
             "Directory Exists",
-            f"The directory '{folder_name}' already exists. Do you want to overwrite it?",
+            f"The directory '{os.path.basename(output_dir)}' already exists. Do you want to overwrite it?",
             QMessageBox.Yes | QMessageBox.No,
         )
         if reply == QMessageBox.No:
-            # If no, return None
-            return None
+            return False
         else:
-            # If yes, delete the existing directory first
             shutil.rmtree(output_dir)
+    return True
 
-    # Open the video file
-    video_capture = cv2.VideoCapture(source_video_path)
-    total_frames = int(video_capture.get(cv2.CAP_PROP_FRAME_COUNT))
-    fps = int(video_capture.get(cv2.CAP_PROP_FPS))
-
-    # Ask the user to input the frame interval
+def get_frame_interval(parent, fps, total_frames):
     interval, ok = QInputDialog.getInt(
         parent,
         parent.tr("Frame Interval"),
@@ -49,50 +41,61 @@ def extract_frames_from_video(parent, source_video_path):
         total_frames,  # maximum value
         1,  # step
     )
-
     if not ok:
-        # If the user cancels the dialog, show a message box and return
-        QMessageBox.warning(
-            parent, "Cancelled", "Frame extraction was cancelled."
-        )
+        QMessageBox.warning(parent, "Cancelled", "Frame extraction was cancelled.")
+    return interval if ok else None
+
+def extract_frames_from_video(parent, source_video_path):
+    output_dir = get_output_directory(source_video_path)
+
+    if not ask_overwrite_directory(parent, output_dir):
+        return None
+
+    video_capture = cv2.VideoCapture(source_video_path)
+    if not video_capture.isOpened():
+        QMessageBox.critical(parent, "Error", "Failed to open video file.")
+        return None
+
+    total_frames = int(video_capture.get(cv2.CAP_PROP_FRAME_COUNT))
+    fps = int(video_capture.get(cv2.CAP_PROP_FPS))
+
+    interval = get_frame_interval(parent, fps, total_frames)
+    if interval is None:
         return None
 
     os.makedirs(output_dir)
 
-    # Decode the video and save frames to the created folder
     progress_dialog = QProgressDialog(
         parent.tr("Extracting frames. Please wait..."),
         parent.tr("Cancel"),
         0,
         total_frames // interval,
+        parent
     )
     progress_dialog.setWindowModality(Qt.WindowModal)
     progress_dialog.setWindowTitle("Progress")
     progress_dialog.setStyleSheet(
         """
-    QProgressDialog QProgressBar {
-        border: 1px solid grey;
-        border-radius: 5px;
-        text-align: center;
-    }
-    QProgressDialog QProgressBar::chunk {
-        background-color: orange;
-    }
-    """
+        QProgressDialog QProgressBar {
+            border: 1px solid grey;
+            border-radius: 5px;
+            text-align: center;
+        }
+        QProgressDialog QProgressBar::chunk {
+            background-color: orange;
+        }
+        """
     )
 
     frame_count = 0
     saved_frame_count = 0
-    base_name = osp.splitext(osp.basename(source_video_path))[0]
     while True:
         ret, frame = video_capture.read()
         if not ret:
             break
 
         if frame_count % interval == 0:
-            frame_filename = osp.join(
-                output_dir, f"{base_name}-{saved_frame_count}.jpg"
-            )
+            frame_filename = os.path.join(output_dir, f"{saved_frame_count:05}.jpg")
             cv2.imwrite(frame_filename, frame)
             saved_frame_count += 1
             progress_dialog.setValue(saved_frame_count)
@@ -104,5 +107,4 @@ def extract_frames_from_video(parent, source_video_path):
     video_capture.release()
     progress_dialog.close()
 
-    # Return the path of the created folder
     return output_dir
